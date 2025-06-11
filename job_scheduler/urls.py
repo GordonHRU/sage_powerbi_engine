60936c12cffb63170a47ada479b7248d025d935f
gordon <a22521816@gmail.com>
2025-06-11 14:13:47 +0800

    change urls

--- job_scheduler/urls.py
+++ job_scheduler/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('job_scheduler/', views.job_scheduler_view, name='job_scheduler'),
-    path('job_scheduler/create_job/', views.create_job_view, name='create_job'),
+    path('', views.job_scheduler_view, name='job_scheduler'),
+    path('create_job/', views.create_job_view, name='create_job'),
     path('create/', views.create_job_view, name='create_job'),
     path('api/job/<int:job_id>/status/', views.get_job_status, name='get_job_status'),
     path('api/job/<int:job_id>/history/', views.get_execution_history, name='get_execution_history'),
