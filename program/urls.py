c5a841292db93202e27ff921a55a551e546e63e6
andy2167565 <andy2167565@gmail.com>
2025-06-12 18:53:30 +0800

    Add program draft

--- program/urls.py
+++ program/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.program_view, name='program'),
+    path('create/', views.create_program_view, name='create_program'),
 ]
--- program/views.py
+++ program/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def program_view(request):
     return render(request, 'program/program.html')
+
+def create_program_view(request):
+    return render(request, 'program/create_program.html')
